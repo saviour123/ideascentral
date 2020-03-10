c2e3faad2224c63822d860bbe9dfa2f53afc0407
saviour gidi <profsaviour@gmail.com>
2020-03-10 12:22:37 +0000

    completed the authentication section with devise gem :tada:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # devise_for :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
 end
