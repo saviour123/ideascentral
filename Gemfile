c4cb54a9d77c60d519e50a7bf42d1119bc6b17d3
saviour gidi <profsaviour@gmail.com>
2020-03-16 13:55:21 +0000

    pg

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'gravtastic'
 gem 'jbuilder', '~> 2.7'
+gem 'pg'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Idea, type: :model do
   end
 
   describe 'validations' do
-    it{ is_expected.to validate_presence_of :name}
-    it{ is_expected.to validate_presence_of :description}
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :description }
   end
 end
